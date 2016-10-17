e6fb2cf33a153ed986f69c7411328a50c52f90ae
Ivan Koshelev <koshelew@outlook.com>
2016-10-18 01:27:17 +0200

    Added missing file

---
+++ Shared/Dtos/ProductGridRow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dtos
+{
+    public class ProductGridRow
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string ProductNumber { get; set; }
+        public string ModelName { get; set; }
+        public string[] Descriptions { get; set; }
+    }
+}
