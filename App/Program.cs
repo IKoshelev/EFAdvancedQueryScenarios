bb35089cc72a0c1aaf7738147a65ab556647e1a3
Ivan Koshelev <koshelew@outlook.com>
2016-10-18 21:30:40 +0200

    Switched to console app.

---
+++ App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new EFDataContext.AdventureWorksDataContext();
+
+            var repo = new Repositories.ProductsRepository(context);
+
+            var srv = new Services.ProductsService(repo);
+
+            var test = repo.GetWorkOrderSummaries().ToArray();
+        }
+    }
+}
